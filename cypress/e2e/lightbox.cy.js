8a0b6827ae41c3935e74e821146f610c31de752c
BastienGuerin <147147977+BastienGuerin@users.noreply.github.com>
2024-06-21 14:00:27 +0200

    commit pull

--- cypress/e2e/lightbox.cy.js
+++ cypress/e2e/lightbox.cy.js
@@ -1,5 +1,8 @@
 describe('test', () => {
     it('passes', () => {
         cy.visit('lightbox.html')
+        cy.get('[data-cy=img]').click();
+        cy.wait(1000)
+        cy.get('[data-cy=close]').click()
     })
 })
